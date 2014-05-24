a6705d95d78bcf49ca6d144a37f3809c81ad0fc9
courtney mcilhenny <github@courtneymcilhenny>
2014-05-24 11:50:10 -0700

    fixing typos and methods to reflect RESTful routes.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,22 +1,41 @@
 class CompaniesController < ApplicationController
+	
 	def index
 		@companies = Company.all
-	 end
-	 def create
-	 	@companies = Company.new(params[:id])
-	 end
-	 def show
-	 	@compaies = Company.all
-	 end
-	 def new
-	 	@companies = Company.all
+
+		respond_to do |f|
+			f.json {render :json => @companies, :except => :created_at}
+		end
+	end
+	 
+	def create
+	 	@company = Company.new(params[:id])
+	end
+	
+	def show
+	 	@company = Company.all
+	end
+	
+	def new
+	 	@company = Company.all
 		respond_to do |f|	
 	 	f.html { render :html }
 	 	f.json { render :json => @companies }
 	 	end
-	 end
-	 def destory
-	 end
+	end
+	
+	def delete
+	
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
 	private
 	def companies_params
 		params.require(:companies).permit(:title, :size, :industry, :location, :description, :url)

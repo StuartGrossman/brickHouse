61af8249467fff20a1e37ee720c91b7a85aa9744
courtney mcilhenny <github@courtneymcilhenny>
2014-05-24 12:06:55 -0700

    updating index and create methods to respond to json. Implimenting destroy, update, edit, new for future use.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,35 +9,44 @@ class CompaniesController < ApplicationController
 	end
 	 
 	def create
-	 	@company = Company.new(params[:id])
-	end
-	
-	def show
-	 	@company = Company.all
-	end
-	
-	def new
-	 	@company = Company.all
-		respond_to do |f|	
-	 	f.html { render :html }
-	 	f.json { render :json => @companies }
+	 	@company = Company.new company_params
+
+	 	respond_to do |f|	
+	 		if @company.save
+	 			f.html { render :html }
+	 			f.json { render :json => @company, status: :created }
+	 		else
+	 			f.json { render json: @company.errors, status: :unprocessable_entity}
+	 		end
 	 	end
 	end
 	
-	def delete
+	def show
+	 	@company = Company.find(params[:id])
+	end
 	
+	def new
+	 	@company = Company.new
+		
+	end
+	
+	def destroy
+		Company.find(params[:id].destroy)
+		redirect_to companies_path
 	end
 
 	def edit
-
+		@company = Company.find(params[:id])
 	end
 
 	def update
-
+		company = Company.find(params[:id])
+		company.update company_params
+		redirect_to company_path
 	end
 
 	private
-	def companies_params
-		params.require(:companies).permit(:title, :size, :industry, :location, :description, :url)
+	def company_params
+		params.require(:company).permit(:title, :size, :industry, :location, :description, :url)
 	end
 end

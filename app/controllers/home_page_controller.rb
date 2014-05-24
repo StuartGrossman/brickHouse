e541ddb14e1859e78f8c8a96dbefb9f91c732171
courtney mcilhenny <github@courtneymcilhenny>
2014-05-24 12:05:52 -0700

    adding a blank search method for the team to discuss user flow.

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,8 @@
 class HomePageController < ApplicationController
 	def home
 	end
+
+  def search
+    # impliment a search function here that allows a user to search for a company listed in our database
+  end
 end
